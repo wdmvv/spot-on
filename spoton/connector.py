4e0fa3681277a399119bd652d5a20fede908f961
~wdmvv <78714307+wdmvv@users.noreply.github.com>
2023-10-12 08:44:13 +0600

    py 3.9

--- spoton/connector.py
+++ spoton/connector.py
@@ -22,21 +22,21 @@ class Connector:
     def process(self, type: str, link: str, download_path: str, precise: bool):
         download_id = self.get_id(link)
 
-        match type:
-            case 'playlist':
-                responses = self.spotify_obj.get_playlist(download_id)
-                tracks = structs.tracks_from_playlist(responses)
-            case 'album':
-                responses = self.spotify_obj.get_album(download_id)
-                album = self.spotify_obj.get_album_info(download_id)
-                tracks = structs.tracks_from_album(responses, album)
-            case _:
-                raise(Exception('Invalid download type'))
+        if type == 'playlist':
+            responses = self.spotify_obj.get_playlist(download_id)
+            tracks = structs.tracks_from_playlist(responses)
+        elif type == 'album':
+            responses = self.spotify_obj.get_album(download_id)
+            album = self.spotify_obj.get_album_info(download_id)
+            tracks = structs.tracks_from_album(responses, album)
+        else:
+            raise(Exception('Invalid download type'))
 
         Downloader = downloader.Downloader(download_path, self.workers)
         Downloader.batch_download(tracks, precise)
 
 
     def get_id(self, link: str):
-        parts = link.split('/')
-        return parts[-1] if parts[-1] != '' else parts[-2]
+        path = link.split('?')[0]
+        parts = path.split('/')
+        return parts[-1] if parts[-1] != '' else parts[-2]    
